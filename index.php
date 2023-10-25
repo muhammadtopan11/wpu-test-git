1f9f0d88da044f790358dc7345a24962f281aa72
TopanKurniawann <muhammadtopan2@gmail.com>
2023-10-25 21:26:08 +0700

    Menambahkan list Topan

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remot</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, in!</p>
+    <ul>
+        <li>Topan</li>
+    </ul>
+</body>
+</html>
